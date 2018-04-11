e1b891a6cca6a4b54e5a4fce8eb2419bc3942caa
braunth <braunth@oregonstate.edu>
2018-04-10 20:42:16 -0700

    First Upload

---
+++ src/com/neuralgis/graphics/GPUMathKernel.java
@@ -0,0 +1,28 @@
+package com.neuralgis.graphics;
+
+import static jcuda.driver.JCudaDriver.cuCtxCreate;
+import static jcuda.driver.JCudaDriver.cuDeviceGet;
+import static jcuda.driver.JCudaDriver.cuInit;
+import static jcuda.driver.JCudaDriver.cuMemAlloc;
+import static jcuda.driver.JCudaDriver.cuMemFree;
+import static jcuda.driver.JCudaDriver.cuMemcpyDtoH;
+import static jcuda.driver.JCudaDriver.cuMemcpyHtoD;
+import jcuda.Pointer;
+import jcuda.Sizeof;
+import jcuda.driver.CUcontext;
+import jcuda.driver.CUdevice;
+import jcuda.driver.CUdeviceptr;
+import jcuda.driver.JCudaDriver;
+import jcuda.vec.VecFloat;
+
+/**
+ *
+ * @author braunth
+ */
+public class GPUMathKernel {
+    
+    public GPUMathKernel(){
+        //hello
+    }
+    
+}
